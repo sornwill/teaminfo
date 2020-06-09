a2ef264edcde61be32c51f48eca6bf25a09990bf
Soul <sornwilliam@gmail.com>
2020-06-09 17:32:13 -0500

    got all inputs for employees, trrying to render to html but it's not recognizing employee.getrole as a function.

--- Develop/app.js
+++ Develop/app.js
@@ -10,62 +10,93 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-let team = [];
+
+let employees = [];
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+init();
 
-getEmployees();
 
 
 
 
 // FUNCTIONS
 
-function EmployeeNumbers(num){
+function init(){  //initializes Introduction of Team App. Gives choices of adding member or rendering files
     inquirer.prompt([
         {
-            type:"input",
-            message:"How Many Employee's are in this Team? (Please Enter an Integer)",
-            name:"num"
+            type:"list",
+            message:"Welcome to the Team App. \n Here you can add employees to your team. You will be asked \n A series of questions and also assigning roles to them.",
+            choices:["Add Employee", "Render File"],
+            name:"main"
         }
     ])
-    .then(function(teamNum){
-        console.log(teamNum);
-        num = teamNum;
+    .then(function(main){
+        switch (main.main) {
+            case "Add Employee":
+                console.log("\nAdding Employee...\n")
+                getEmployees();
+                break;
+            case "Render File":
+                console.log("\nRendering File...\n");
+                render(employees);
+                break;
+        }
     })
-};
+}
 
-function forLoop(){
-    
+function addMore(){   // Same as above but with different message
+    inquirer.prompt([
+        {
+            type:"list",
+            message:"Would you like to add more Employees?",
+            choices:["Add Employee", "Render File"],
+            name:"main"
+        }
+    ])
+    .then(function(main){
+        switch (main.main) {
+            case "Add Employee":
+                console.log("\n Adding Employee...\n")
+                getEmployees();
+                break;
+            case "Render File":
+                console.log("\n Rendering File...\n");
+                console.log(employees);
+                render(employees);
+                break;
+        }
+    })
 }
 
 function getEmployees(){ // Get's employee name, id, email via inquirer then depending on choice of manager, engineer, or intern, it'll ask the prompts depending on role.
     inquirer.prompt([
         {
             type:"input",
-            message:"What is the Employee's Name?",
+            message:"What is the Employee's Name? \n (Please Insert First and Last Name)\n",
             name:"name"
         },
         {
             type:"input",
-            message:"What is their ID?",
+            message:"What is their Employee ID?\n",
             name:"id"
         },
         {
             type:"input",
-            message:"What is their E-mail?",
+            message:"What is their E-mail?\n",
             name:"email"
         },
         {
             type:"checkbox",
-            message:"What is their role in this team?",
+            message:"What is their role in this team?\n",
             choices:["Manager", "Engineer", "Intern"],
             name:"role"
         },
         {
             type:"input",
-            message:"What is the Manager's Office #?",
+            message:"What is the Manager's Office #?\n",
             name:"officeNumber",
             when: function(answers){
                 return answers.role == "Manager";
@@ -73,7 +104,7 @@ function getEmployees(){ // Get's employee name, id, email via inquirer then dep
         },
         {
             type:"input",
-            message:"What is the Intern's School?",
+            message:"What is the Intern's School?\n",
             name:"school",
             when: function(answers){
                 return answers.role == "Intern";
@@ -81,7 +112,7 @@ function getEmployees(){ // Get's employee name, id, email via inquirer then dep
         },
         {
             type:"input",
-            message:"What is the Engineer's GitHub Name?",
+            message:"What is the Engineer's GitHub Name?\n",
             name:"github",
             when: function(answers){
                 return answers.role == "Engineer";
@@ -90,11 +121,18 @@ function getEmployees(){ // Get's employee name, id, email via inquirer then dep
         
     ])
     .then(function(info){
-        console.log(info);
-
-        
+        // if(info.role === "Manager"){
+        //     const manager = new Manager(
+        //         info.name,
+        //         info.id,
+        //         info.email,
+        //         info.officeNumber
+        //         )
+        //     employees.push(manager);
+        // }
+        employees.push(info);
+        addMore();
     })
-
 };
 
 
